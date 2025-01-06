bb7b831bb2a94aa6bb152e86ea78904e7f1f6277
john-kamau01 <johnwaweru092@gmail.com>
2025-01-06 21:33:31 +0300

    updates

--- server.js
+++ server.js
@@ -18,3 +18,5 @@ app.use("/api/reminders", require("./routes/reminderRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
