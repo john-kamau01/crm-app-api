c9ad2a8a1c6f2a914d320910fab0d4f6df094acf
john-kamau01 <johnwaweru092@gmail.com>
2025-01-06 21:26:02 +0300

    updates

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`DB connected: ${(await conn).Connection.host}`);
+    console.log(`DB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
